3049072f2b7eccc29b3cb37e21e1b7f16788eb5b
Silvinsk <81040770+Almir-git-unifc@users.noreply.github.com>
2022-05-24 18:05:06 -0400

    feat(json): v01.01.000 - react create struct of project with title and question

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import QuestionsList from "./components/QuestionsList";
+import { v4 as uuidv4 } from "uuid";
+import "./App.css";
+
+const App = () => {
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [score, setScore] = useState(0);
+
+  return (
+    <div className="app-wrapper">
+      <div>
+        <div className="question-section-wrapper">
+          <div className="question-count">
+            Question{currentQuestion + 1} of {QuestionsList.length}
+          </div>
+          <div className="question">
+            {QuestionsList[currentQuestion].question}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/QuestionsList.js
@@ -0,0 +1,48 @@
+const QuestionsList = [
+  {
+    question: "Almir é o dono deste aplicativo ?",
+    answersList: [
+      { answer: "Não sei", isCorrect: false },
+      { answer: "Sim", isCorrect: true },
+      { answer: "Vou pensar", isCorrect: false },
+      { answer: "Claro, que não", isCorrect: false },
+    ],
+  },
+  {
+    question: "What is the full form of VWW ?",
+    answersList: [
+      { answer: "World Width Web", isCorrect: false },
+      { answer: "World War Won", isCorrect: false },
+      { answer: "World Wide Web", isCorrect: true },
+      { answer: "World Web Wide", isCorrect: false },
+    ],
+  },
+  {
+    question: "What is the highest peak in the world ?",
+    answersList: [
+      { answer: "Mount Everest", isCorrect: true },
+      { answer: "Mount K2", isCorrect: false },
+      { answer: "Annapurna", isCorrect: false },
+      { answer: "Makalu", isCorrect: false },
+    ],
+  },
+  {
+    question: "Who is the highest earning Youtuber in 2020 ?",
+    answersList: [
+      { answer: "Mr.Beast", isCorrect: false },
+      { answer: "David Dobrik", isCorrect: false },
+      { answer: "Dude Perfect", isCorrect: false },
+      { answer: "Ryan Kaji", isCorrect: true },
+    ],
+  },
+  {
+    question: "Who maintains React ?",
+    answersList: [
+      { answer: "Google", isCorrect: false },
+      { answer: "Amazon", isCorrect: false },
+      { answer: "Facebook", isCorrect: true },
+      { answer: "Apple", isCorrect: false },
+    ],
+  },
+];
+export default QuestionsList;
