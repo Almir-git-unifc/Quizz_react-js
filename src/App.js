b7bdd60f0d23b47e6f5ee85e27a13e69a16f93bb
Silvinsk <81040770+Almir-git-unifc@users.noreply.github.com>
2022-05-25 00:31:29 -0400

    feat(json): v05.01.000 - react, answer button torned colored when clicked

--- src/App.js
+++ src/App.js
@@ -6,29 +6,65 @@ import "./App.css";
 const App = () => {
   const [currentQuestion, setCurrentQuestion] = useState(0);
   const [score, setScore] = useState(0);
+  const [clicked, setClicked] = useState(false);
+  const [showScore, setShowScore] = useState(false);
+
+  const handleCorrectAnswer = (isCorrect) => {
+    if (isCorrect) {
+      setScore(score + 1);
+    }
+    setClicked(true);
+  };
+
+  const handleNextQuestion = () => {
+    setClicked(false);
+    if (currentQuestion < QuestionsList.length - 1) {
+      setCurrentQuestion(currentQuestion + 1);
+    } else {
+      setShowScore(true);
+    }
+  };
 
   return (
     <div className="app-wrapper">
-      <div>
-        <div className="question-section-wrapper">
-          <div className="question-count">
-            Question{currentQuestion + 1} of {QuestionsList.length}
-          </div>
-          <div className="question">
-            {QuestionsList[currentQuestion].question}
+      {showScore ? (
+        <div>
+          <div className="completed">Fim do Jogo!</div>
+          <div className="score-section">
+            Pontuação: {score}/{QuestionsList.length}
           </div>
         </div>
-
-        <div className="answer-section-wrapper">
-          {QuestionsList[currentQuestion].answersList.map((answerOption) => (
-            <li className="answer-list" key={uuidv4()}>
-              <button className="answer-button">{answerOption.answer}</button>
-            </li>
-          ))}
+      ) : (
+        <div>
+          <div className="question-section-wrapper">
+            <div className="question-count">
+              Questão {currentQuestion + 1} of {QuestionsList.length}
+            </div>
+            <div className="question">
+              {QuestionsList[currentQuestion].question}
+            </div>
+          </div>
+          <div className="answer-section-wrapper">
+            {QuestionsList[currentQuestion].answersList.map((answerOption) => (
+              <li className="answer-list" key={uuidv4()}>
+                <button
+                  className={`answer-button ${
+                    clicked && answerOption.isCorrect ? "correct" : "" 
+                  }`}
+                  onClick={() => handleCorrectAnswer(answerOption.isCorrect)}
+                >
+                  {answerOption.answer}
+                </button>
+              </li>
+            ))}
+          </div>
+          <div>
+            <button className="next-button" onClick={handleNextQuestion}>
+              Próximo
+            </button>
+          </div>
         </div>
-      </div>
-      <button className="next-button">Next</button>
-      <div></div>
+      )}
     </div>
   );
 };
--- src/components/QuestionsList.js
+++ src/components/QuestionsList.js
@@ -2,23 +2,23 @@ const QuestionsList = [
   {
     question: "Almir é o dono deste aplicativo ?",
     answersList: [
-      { answer: "VERSÃO 3", isCorrect: false },
+      { answer: "VERSÃO 5", isCorrect: false },
       { answer: "SIM", isCorrect: true },
       { answer: "Não sei", isCorrect: false },
       { answer: "Vou pensar", isCorrect: false },
     ],
   },
   {
-    question: "What is the full form of VWW ?",
+    question: "Quem criou o Linux ?",
     answersList: [
-      { answer: "World Width Web", isCorrect: false },
-      { answer: "World War Won", isCorrect: false },
-      { answer: "World Wide Web", isCorrect: true },
-      { answer: "World Web Wide", isCorrect: false },
+      { answer: "Mark Zuckerberg", isCorrect: false },
+      { answer: "Mark Shuttleworth", isCorrect: false },
+      { answer: "Linus Torvalds", isCorrect: true },
+      { answer: "Bob Young", isCorrect: false },
     ],
   },
   {
-    question: "What is the highest peak in the world ?",
+    question: "Qual é o maior pico do mundo ?",
     answersList: [
       { answer: "Mount Everest", isCorrect: true },
       { answer: "Mount K2", isCorrect: false },
@@ -27,16 +27,16 @@ const QuestionsList = [
     ],
   },
   {
-    question: "Who is the highest earning Youtuber in 2020 ?",
+    question: "Quem foi o criador do WWW ?",
     answersList: [
-      { answer: "Mr.Beast", isCorrect: false },
-      { answer: "David Dobrik", isCorrect: false },
-      { answer: "Dude Perfect", isCorrect: false },
-      { answer: "Ryan Kaji", isCorrect: true },
+      { answer: "Larry Ellison", isCorrect: false },
+      { answer: "Steve Jobs", isCorrect: false },
+      { answer: "Bill Gates", isCorrect: false },
+      { answer: "Tim Berners-Lee", isCorrect: true },
     ],
   },
   {
-    question: "Who maintains React ?",
+    question: "Quem é o mantededor do React ?",
     answersList: [
       { answer: "Google", isCorrect: false },
       { answer: "Amazon", isCorrect: false },
